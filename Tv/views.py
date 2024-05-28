1c4d8179a7080f53a235ccf3a6b1ea623e69232c
cjiten <cjiten15@gmail.com>
2024-05-28 11:05:38 +0530

    view commented

--- Tv/views.py
+++ Tv/views.py
@@ -130,8 +130,8 @@ def SeriesAddView(request):
             for season_data in seasons:
                 messages.success(request, 'no error here 003')
                 season_number = season_data.get("season_number")
-                if season_number:
-                    add_season(tmdb_id, season_number, org.tmdb_token)
+                # if season_number:
+                    # add_season(tmdb_id, season_number, org.tmdb_token)
         else:
             messages.error(request, 'Request was not successful. Status code:', series_details.status_code)
 
@@ -139,89 +139,89 @@ def SeriesAddView(request):
         return render(request, 'Tv/SeriesAddView.html')
     return render(request, 'Tv/SeriesAddView.html')
 
-def add_season(tmdb_id, season_number, tmdb_token):
-    reqUrl = f"https://api.themoviedb.org/3/tv/{tmdb_id}/season/{season_number}?language=en-US"
+# def add_season(tmdb_id, season_number, tmdb_token):
+#     reqUrl = f"https://api.themoviedb.org/3/tv/{tmdb_id}/season/{season_number}?language=en-US"
     
-    headersList = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {tmdb_token}"
-    }
+#     headersList = {
+#         "accept": "application/json",
+#         "Authorization": f"Bearer {tmdb_token}"
+#     }
 
-    response = requests.get(reqUrl, headers=headersList)
+#     response = requests.get(reqUrl, headers=headersList)
     
-    if response.status_code == 200:
-        data = response.json()
-        series = get_object_or_404(Series, tmdb_id=tmdb_id)
+#     if response.status_code == 200:
+#         data = response.json()
+#         series = get_object_or_404(Series, tmdb_id=tmdb_id)
 
-        img_path = data.get("poster_path")
-        if img_path:
-            poster_path = f"media/posters/tv/season/{tmdb_id}{img_path}"
-            if not os.path.exists(poster_path):
-                poster_url = f"https://image.tmdb.org/t/p/original{img_path}"
-                poster_response = requests.get(poster_url)
-                if poster_response.status_code == 200:
-                    os.makedirs(os.path.dirname(poster_path), exist_ok=True)
-                    with open(poster_path, 'wb') as f:
-                        f.write(poster_response.content)
+#         img_path = data.get("poster_path")
+#         if img_path:
+#             poster_path = f"media/posters/tv/season/{tmdb_id}{img_path}"
+#             if not os.path.exists(poster_path):
+#                 poster_url = f"https://image.tmdb.org/t/p/original{img_path}"
+#                 poster_response = requests.get(poster_url)
+#                 if poster_response.status_code == 200:
+#                     os.makedirs(os.path.dirname(poster_path), exist_ok=True)
+#                     with open(poster_path, 'wb') as f:
+#                         f.write(poster_response.content)
 
-        update_season = Season.objects.filter(series=series, season_number=season_number).first()
+#         update_season = Season.objects.filter(series=series, season_number=season_number).first()
         
-        if update_season:
-            update_season.title = data.get("name", "")
-            update_season.overview = data.get("overview", "")
-            update_season.release_date = data.get("air_date", "")
-            update_season.poster_path = f"posters/tv/season/{tmdb_id}{img_path}"
-            update_season.save()
-        else:
-            Season.objects.create(
-                series=series,
-                title=data.get("name", ""),
-                overview=data.get("overview", ""),
-                season_number=season_number,
-                release_date=data.get("air_date", ""),
-                poster_path=f"posters/tv/season/{tmdb_id}{img_path}"
-            )
-        # Add seasons for the series
-        episodes = data.get("episodes", [])
-        for episode_data in episodes:
-            episode_data
-            if episode_data:
-                add_episode(series, episode_data, season_number, tmdb_id)
+#         if update_season:
+#             update_season.title = data.get("name", "")
+#             update_season.overview = data.get("overview", "")
+#             update_season.release_date = data.get("air_date", "")
+#             update_season.poster_path = f"posters/tv/season/{tmdb_id}{img_path}"
+#             update_season.save()
+#         else:
+#             Season.objects.create(
+#                 series=series,
+#                 title=data.get("name", ""),
+#                 overview=data.get("overview", ""),
+#                 season_number=season_number,
+#                 release_date=data.get("air_date", ""),
+#                 poster_path=f"posters/tv/season/{tmdb_id}{img_path}"
+#             )
+#         # Add seasons for the series
+#         episodes = data.get("episodes", [])
+#         for episode_data in episodes:
+#             episode_data
+#             if episode_data:
+#                 add_episode(series, episode_data, season_number, tmdb_id)
 
 
-def add_episode(series, episode_data, season_number, tmdb_id):
-    season = get_object_or_404(Season, series=series, season_number=season_number)
-    episode_number = episode_data.get("episode_number")
+# def add_episode(series, episode_data, season_number, tmdb_id):
+#     season = get_object_or_404(Season, series=series, season_number=season_number)
+#     episode_number = episode_data.get("episode_number")
 
-    img_path = episode_data.get("still_path")
-    if img_path:
-        poster_path = f"media/posters/tv/episode/{tmdb_id}{img_path}"
-        if not os.path.exists(poster_path):
-            poster_url = f"https://image.tmdb.org/t/p/original{img_path}"
-            poster_response = requests.get(poster_url)
-            if poster_response.status_code == 200:
-                os.makedirs(os.path.dirname(poster_path), exist_ok=True)
-                with open(poster_path, 'wb') as f:
-                    f.write(poster_response.content)
+#     img_path = episode_data.get("still_path")
+#     if img_path:
+#         poster_path = f"media/posters/tv/episode/{tmdb_id}{img_path}"
+#         if not os.path.exists(poster_path):
+#             poster_url = f"https://image.tmdb.org/t/p/original{img_path}"
+#             poster_response = requests.get(poster_url)
+#             if poster_response.status_code == 200:
+#                 os.makedirs(os.path.dirname(poster_path), exist_ok=True)
+#                 with open(poster_path, 'wb') as f:
+#                     f.write(poster_response.content)
 
-    update_episode = Episode.objects.filter(season=season, episode_number=episode_number).first()
+#     update_episode = Episode.objects.filter(season=season, episode_number=episode_number).first()
         
-    if update_episode:
-        update_episode.season = season
-        update_episode.title = episode_data.get("name", "")
-        update_episode.overview = episode_data.get("overview", "")
-        update_episode.runtime = episode_data.get("runtime", "")
-        update_episode.episode_number = episode_data.get("episode_number", "")
-        update_episode.release_date = episode_data.get("air_date", "")
-        update_episode.poster_path = f"posters/tv/episode/{tmdb_id}{img_path}"
-        update_episode.save()
-    else:
-        Episode.objects.create(
-            season = season,
-            title = episode_data.get("name", ""),
-            overview = episode_data.get("overview", ""),
-            runtime = episode_data.get("runtime", ""),
-            episode_number = episode_data.get("episode_number", ""),
-            release_date = episode_data.get("air_date", ""),
-            poster_path = f"posters/tv/episode/{tmdb_id}{img_path}"
-        )
+#     if update_episode:
+#         update_episode.season = season
+#         update_episode.title = episode_data.get("name", "")
+#         update_episode.overview = episode_data.get("overview", "")
+#         update_episode.runtime = episode_data.get("runtime", "")
+#         update_episode.episode_number = episode_data.get("episode_number", "")
+#         update_episode.release_date = episode_data.get("air_date", "")
+#         update_episode.poster_path = f"posters/tv/episode/{tmdb_id}{img_path}"
+#         update_episode.save()
+#     else:
+#         Episode.objects.create(
+#             season = season,
+#             title = episode_data.get("name", ""),
+#             overview = episode_data.get("overview", ""),
+#             runtime = episode_data.get("runtime", ""),
+#             episode_number = episode_data.get("episode_number", ""),
+#             release_date = episode_data.get("air_date", ""),
+#             poster_path = f"posters/tv/episode/{tmdb_id}{img_path}"
+#         )
