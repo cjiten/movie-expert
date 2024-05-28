b74baea1fdf5458c8da555f8987c9f2e8dd57809
cjiten <cjiten15@gmail.com>
2024-05-28 10:39:40 +0530

    View Change

--- Tv/views.py
+++ Tv/views.py
@@ -63,7 +63,8 @@ def SeriesAddView(request):
         payload = ""
 
         series_details = requests.request("GET", reqUrl, data=payload,  headers=headersList)
-        messages.success(request, 'Series Created Successfully', series_details)
+        messages.success(request, 'Request was successful. Status code:', series_details.status_code)
+        messages.success(request, 'Request was successful. Status code:', series_details.json())
         update_series = Series.objects.filter(tmdb_id=tmdb_id).first()
 
         if series_details.status_code == 200:
