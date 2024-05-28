167924af15f5d156d033aca353ca832b3fb6ffac
cjiten <cjiten15@gmail.com>
2024-05-28 10:43:32 +0530

    First Commit

--- Tv/views.py
+++ Tv/views.py
@@ -63,8 +63,8 @@ def SeriesAddView(request):
         payload = ""
 
         series_details = requests.request("GET", reqUrl, data=payload,  headers=headersList)
-        messages.success(request, 'Request was successful. Status code:', series_details.status_code)
         messages.success(request, 'Request was successful. Status code:', series_details)
+        messages.success(request, 'Request was successful. Status code:', series_details.status_code)
         update_series = Series.objects.filter(tmdb_id=tmdb_id).first()
 
         if series_details.status_code == 200:
